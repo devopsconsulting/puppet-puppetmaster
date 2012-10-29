9ab1a25c15242951e39c884fcaab3aa97563f918
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-29 11:39:14 +0100

    Cleanued up puppetmaster package, removed unneeded ugly apache hack

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,8 @@ class puppetmaster {
     include puppetmaster::version
     include puppetmaster::mysqldb
     include puppetmaster::rails
+    include puppetmaster::passenger
+    include puppetmaster::deploy
     Class["puppetmaster::host"] -> Class["puppetmaster::version"] -> Class["puppetmaster::mysqldb"] ->
-    Class["puppetmaster::rails"]
+    Class["puppetmaster::rails"] -> Class["puppetmaster::passenger"] -> Class["puppetmaster::deploy"]
 }
