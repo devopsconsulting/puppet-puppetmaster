a31d87f7ca2ab6cddcf523a12d4634c79822de0e
Martijn Jacobs <martijn@kamaramusic.nl>
2013-01-23 09:46:11 +0000

    Added some comments

--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -11,11 +11,13 @@ class puppetmaster::foreman {
         mode => "755",
     } ->
 
+    # execute the migration directly after the creation of the db, otherwise it will fail
     exec { "migrate-db":
          command => "/bin/su - foreman -s /bin/bash -c /usr/share/foreman/extras/dbmigrate",
          cwd => "/usr/share/foreman/",
     } ->
 
+    # this patch will show the role fact of the machine
    file {"/usr/share/foreman/app/views/hosts/_list.html.erb":
         source => 'puppet:///modules/puppetmaster/usr/share/foreman/app/views/hosts/_list.html.erb',
         ensure => file,
@@ -25,6 +27,7 @@ class puppetmaster::foreman {
         mode => '0644',
     } ->
 
+    # make sure the report get's into foreman
    file {"/usr/lib/ruby/1.8/puppet/reports/foreman.rb":
         source => 'puppet:///modules/puppetmaster/foreman.rb',
         ensure => file,
