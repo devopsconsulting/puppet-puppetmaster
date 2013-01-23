d3773e1857d27e9bd3dade7e2c910a2d7d595732
Martijn Jacobs <martijn@kamaramusic.nl>
2013-01-23 09:42:15 +0000

    Directly include foreman after creating the mysql db to make sure the migration won't fail

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class puppetmaster {
     class {"puppetmaster::host": } -> 
     class {"puppetmaster::version": } ->
     class {"puppetmaster::mysqldb": } ->
+    class {"puppetmaster::foreman": } ->
     class {"puppetmaster::rails": } -> 
     class {"puppetmaster::passenger": } -> 
-    class {"puppetmaster::deploy": } ->
-    class {"puppetmaster::foreman": }
+    class {"puppetmaster::deploy": }
 }
