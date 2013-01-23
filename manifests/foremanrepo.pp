0aaae6e0b328dbb573043da8079790e59a4c1431
Martijn Jacobs <martijn@kamaramusic.nl>
2013-01-23 09:41:35 +0000

    Use nightly build as the stable build is really, really old

--- manifests/foremanrepo.pp
+++ manifests/foremanrepo.pp
@@ -2,7 +2,7 @@ class puppetmaster::foremanrepo {
     apt::source {"foreman":
         location => 'http://deb.theforeman.org/',
         release => 'precise',
-        repos => 'stable',
+        repos => 'nightly',
         key => 'E775FF07',
         key_source => 'http://deb.theforeman.org/foreman.asc',
         include_src => false,
